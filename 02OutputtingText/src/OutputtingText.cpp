ed0344523625ab781fc0cad1ca3bc089c967a495
ericsacchetto <ericsacchetto@gmail.com>
2018-09-05 20:32:22 +1200

    First

---
+++ 02OutputtingText/src/OutputtingText.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Starting programming..." << flush;
+	cout << "!!!Hello World!!!" << endl;
+	cout << "Banana " << "Apple " << "Orange" << endl;
+	cout << "This is some more text" << endl;
+	return 0;
+}
